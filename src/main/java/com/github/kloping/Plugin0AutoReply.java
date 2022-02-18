75ba86c41980f780a27434360a433b7ae2adf8be
HRS-Computer <3474006766@qq.com>
2022-02-18 22:36:16 +0800

    v0.4

--- src/main/java/com/github/kloping/Plugin0AutoReply.java
+++ src/main/java/com/github/kloping/Plugin0AutoReply.java
@@ -22,7 +22,7 @@ public class Plugin0AutoReply extends JavaPlugin {
     public static final Plugin0AutoReply INSTANCE = new Plugin0AutoReply();
 
     private Plugin0AutoReply() {
-        super(new JvmPluginDescriptionBuilder("com.hrs.kloping.AutoReply", "0.4-beta1")
+        super(new JvmPluginDescriptionBuilder("com.hrs.kloping.AutoReply", "0.4")
                 .name("Custom Reply")
                 .info("Custom Reply")
                 .author("HRS")
