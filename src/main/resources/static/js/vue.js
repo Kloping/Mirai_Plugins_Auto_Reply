aee94d9ed18291bb7e82456e57ab89d333618382
HRS-Computer <3474006766@qq.com>
2022-06-04 11:08:30 +0800

    v0.5.1

--- src/main/resources/static/js/vue.js
+++ src/main/resources/static/js/vue.js
@@ -1,8 +1,4 @@
-/*!
- * Vue.js v2.6.14
- * (c) 2014-2021 Evan You
- * Released under the MIT License.
- */
+
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
         typeof define === 'function' && define.amd ? define(factory) :
