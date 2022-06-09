523c56437fb127feb037214be90bcfa3851a880a
HRS-Computer <3474006766@qq.com>
2022-06-09 18:51:17 +0800

    v0.5.2

--- src/main/java/com/github/kloping/Plugin0AutoReply.java
+++ src/main/java/com/github/kloping/Plugin0AutoReply.java
@@ -22,7 +22,7 @@ public class Plugin0AutoReply extends JavaPlugin {
     public static final Plugin0AutoReply INSTANCE = new Plugin0AutoReply();
 
     private Plugin0AutoReply() {
-        super(new JvmPluginDescriptionBuilder("com.hrs.kloping.AutoReply", "0.5.1")
+        super(new JvmPluginDescriptionBuilder("com.hrs.kloping.AutoReply", "0.5.2")
                 .name("Custom Reply")
                 .info("Custom Reply")
                 .author("HRS")
--- src/main/java/com/github/kloping/sp/Starter.java
+++ src/main/java/com/github/kloping/sp/Starter.java
@@ -2,9 +2,8 @@ package com.github.kloping.sp;
 
 import com.github.kloping.Plugin0AutoReply;
 import com.github.kloping.Resource;
-import io.github.kloping.MySpringTool.StarterApplication;
+import io.github.kloping.MySpringTool.StarterObjectApplication;
 import io.github.kloping.MySpringTool.annotations.CommentScan;
-import io.github.kloping.little_web.WebExtension;
 import io.github.kloping.little_web.conf.TomcatConfig;
 
 import static com.github.kloping.Resource.uuid;
@@ -16,9 +15,16 @@ import static com.github.kloping.Resource.uuid;
 public class Starter {
     public static void main(String[] args) {
         try {
-            TomcatConfig.DEFAULT.setPort(Resource.conf.getPort());
-            StarterApplication.SCAN_LOADER = Plugin0AutoReply.class.getClassLoader();
-            StarterApplication.run(Starter.class);
+            TomcatConfig config = new TomcatConfig();
+            config.setPort(Resource.conf.getPort());
+            config.setName("autoReply-web");
+            StarterObjectApplication application = new StarterObjectApplication();
+            application.PRE_SCAN_RUNNABLE.add(() -> {
+                application.INSTANCE.getContextManager().append(config);
+                application.INSTANCE.getContextManager().append("autoReply-servlet0", "servletName");
+            });
+            application.SCAN_LOADER = Plugin0AutoReply.class.getClassLoader();
+            application.run0(Starter.class);
             Plugin0AutoReply.INSTANCE.getLogger().info("AutoReply 服务启动成功 address: http://localhost:" + Resource.conf.getPort() + "?key=" + uuid);
         } catch (Throwable e) {
             System.err.println("AutoReply服务启动异常(请检查端口是否被占用)");
